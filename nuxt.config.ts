77e75acf65049a6040e22524eb87a4d980fe3ba2
Matt Crandell <mrcrandell@gmail.com>
2024-10-07 21:34:02 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,9 @@ export default defineNuxtConfig({
         scss: {
           api: 'modern-compiler', // or "modern"
           additionalData: `
+            @use 'sass:math';
+            @use 'sass:color';
+            @use 'sass:map';
             @use "@/assets/scss/_functions.scss" as *;
             @use "@/assets/scss/_variables.scss" as *;
             @use "@/assets/scss/_mixins.scss" as *;
